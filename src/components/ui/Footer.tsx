2b873119e3cf267260f8de111b678cdd3e8e5d65
Villaruel Jerome <jeromevillaruel1998@gmail.com>
2023-12-29 12:20:42 +0800

    feat: add footer component

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import tw from '../../styles/tailwind';
+
+function Footer() {
+  return (
+    <View style={tw`flex-col items-center w-full p-3 border-t border-accent-2`}>
+      <Text style={tw`font-poppins text-[9px] text-neutral-500`}>
+        &copy; 2023 Papino, All rights reserved.
+      </Text>
+      <Text style={tw`font-poppins text-[9px] text-neutral-500`}>
+        Developed by <Text style={tw`font-poppins-bold text-neutral-500`}>VEOSCRIPT</Text>.
+      </Text>
+    </View>
+  );
+}
+
+export default Footer;
