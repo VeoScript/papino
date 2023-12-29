73a161b89b2136e43d6bf4fafef24be919fa25a3
Villaruel Jerome <jeromevillaruel1998@gmail.com>
2023-12-29 16:14:12 +0800

    feat: add save modal to add a custom filename

---
+++ src/components/ui/Modals/SavePDFModal.tsx
@@ -0,0 +1,76 @@
+import React, {memo, useCallback} from 'react';
+import Modal from 'react-native-modal';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+
+import tw from '../../../styles/tailwind';
+
+import {useCheckKeyboard} from '../../../hooks/useCheckKeyboard';
+
+interface SavePDFModalProps {
+  isSaving?: boolean;
+  fileName: string;
+  setFileName: (value: string) => void;
+  isVisible: boolean;
+  setIsVisible: (value: boolean) => void;
+  onSave: () => void;
+}
+
+function SavePDFModal({
+  isSaving,
+  fileName,
+  setFileName,
+  isVisible,
+  setIsVisible,
+  onSave,
+}: SavePDFModalProps) {
+  const onClose = useCallback(() => {
+    setIsVisible(false);
+  }, [setIsVisible]);
+
+  const isKeyboardVisible = useCheckKeyboard();
+
+  return (
+    <Modal
+      statusBarTranslucent
+      avoidKeyboard
+      isVisible={isVisible}
+      onBackdropPress={onClose}
+      onBackButtonPress={onClose}
+      onSwipeMove={onClose}
+      swipeDirection="down"
+      backdropOpacity={0}
+      style={tw`m-0`}>
+      <View
+        style={tw.style(
+          isKeyboardVisible ? 'h-[30rem]' : 'h-[15rem]',
+          'absolute bottom-0 flex-col items-center w-full gap-y-3 rounded-t-3xl border-2 border-accent-3 bg-accent-4',
+        )}>
+        <View style={tw`w-[5rem] h-2 mt-2 rounded-full bg-accent-1`} />
+        <View style={tw`flex-col items-center w-full p-3 gap-y-3`}>
+          <Text style={tw`font-poppins-bold text-base text-neutral-700`}>Save as PDF</Text>
+          <View style={tw`flex-col w-full gap-y-1`}>
+            <Text style={tw`ml-1 font-poppins text-xs text-neutral-700`}>File name</Text>
+            <TextInput
+              style={tw`w-full p-3 font-poppins text-xs rounded-xl text-neutral-700 bg-white`}
+              value={fileName}
+              onChangeText={(value: string) => setFileName(value)}
+            />
+          </View>
+          <TouchableOpacity
+            disabled={isSaving}
+            style={tw.style(
+              isSaving && 'opacity-50',
+              'flex-row items-center justify-center w-full p-3.5 gap-x-2 rounded-xl shadow-md shadow-red-600 bg-accent-1',
+            )}
+            onPress={onSave}>
+            <Text style={tw`font-poppins text-sm text-neutral-700`}>
+              {isSaving ? 'Saving...' : 'Save'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default memo(SavePDFModal);
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -11,21 +11,46 @@ import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import DefaultLayout from '../components/layouts/DefaultLayout';
 import Loading from '../components/ui/Loading';
 import TextEditor from '../components/ui/TextEditor';
+import SavePDFModal from '../components/ui/Modals/SavePDFModal';
 
 function HomeScreen(): JSX.Element {
-  const [isSavingPDF, setIsSavingPDF] = useState<boolean>(false);
+  // for global states...
   const [isGenerating, setIsGenerating] = useState<boolean>(false);
   const [capturedImage, setCapturedImage] = useState<string>('');
   const [recognizedTexts, setRecognizedTexts] = useState<any>(null);
   const [finalTextValue, setFinalTextValue] = useState<string>('');
+
+  // for save pdf modal states...
+  const [isSavingPDF, setIsSavingPDF] = useState<boolean>(false);
+  const [isVisibleSaveModal, setIsVisibleSaveModal] = useState<boolean>(false);
+  const [fileName, setFileName] = useState<string>('');
 
   useEffect(() => {
     setFinalTextValue(recognizedTexts?.text);
   }, [recognizedTexts?.text]);
 
   const handleResetAll = useCallback(() => {
-    setCapturedImage('');
-    setRecognizedTexts(null);
+    Alert.alert(
+      'Clear All',
+      'Are you sure you want to clear all of generated results? This cannot be undone.',
+      [
+        {
+          text: 'No',
+          style: 'cancel',
+        },
+        {
+          text: 'Yes',
+          style: 'default',
+          onPress: () => {
+            setCapturedImage('');
+            setRecognizedTexts(null);
+          },
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
   }, []);
 
   const richTextHandle = useCallback((descriptionText: string): void => {
@@ -86,89 +111,104 @@ function HomeScreen(): JSX.Element {
   }, []);
 
   const handleSave = useCallback(async (): Promise<void> => {
-    setIsSavingPDF(true);
-    try {
-      const options = {
-        html: finalTextValue,
-        fileName: 'papilo_file',
-        directory: 'PapiloFiles',
-      };
-      const file = await RNHTMLtoPDF.convert(options);
-      Alert.alert('Success', `PDF saved to ${file.filePath}`);
-      setIsSavingPDF(false);
-    } catch (error: any) {
-      setIsSavingPDF(false);
-      Alert.alert('Error', error.message);
+    if (fileName !== '') {
+      setIsSavingPDF(true);
+      try {
+        const options = {
+          html: finalTextValue,
+          fileName: fileName,
+          directory: 'PapiloFiles',
+        };
+        const file = await RNHTMLtoPDF.convert(options);
+        Alert.alert('Success', `PDF saved to ${file.filePath}`);
+        setIsSavingPDF(false);
+        setFileName('');
+        setIsVisibleSaveModal(false);
+      } catch (error: any) {
+        setIsSavingPDF(false);
+        Alert.alert('Error', error.message);
+      }
+    } else {
+      Alert.alert('Warning', 'File name is required.');
     }
-  }, [finalTextValue]);
+  }, [fileName, finalTextValue]);
 
   return (
     <>
       {isGenerating ? (
         <Loading />
       ) : (
-        <DefaultLayout>
-          <View style={tw`flex-col items-center w-full my-5 px-5 gap-y-3`}>
-            {capturedImage ? (
-              <>
-                {recognizedTexts?.text === '' ? (
-                  <Text style={tw`px-3 font-poppins text-xs text-center text-red-600`}>
-                    There's no words/paragraph detected in this image! Try again.
-                  </Text>
-                ) : (
-                  <Text style={tw`px-3 font-poppins text-xs text-center text-green-600`}>
-                    Generated Successfully
-                  </Text>
-                )}
-                <ImageBackground
-                  style={tw`relative w-full overflow-hidden rounded-xl shadow-md shadow-red-600`}
-                  resizeMode="cover"
-                  source={{
-                    uri: capturedImage,
-                  }}>
-                  <View style={tw`absolute w-full h-full bg-black opacity-90`} />
-                  <Image
-                    style={tw`w-full h-[26.7rem]`}
-                    resizeMode="contain"
+        <>
+          <DefaultLayout>
+            <View style={tw`flex-col items-center w-full my-5 px-5 gap-y-3`}>
+              {capturedImage ? (
+                <>
+                  {recognizedTexts?.text === '' ? (
+                    <Text style={tw`px-3 font-poppins text-xs text-center text-red-600`}>
+                      There's no words/paragraph detected in this image! Try again.
+                    </Text>
+                  ) : (
+                    <Text style={tw`px-3 font-poppins text-xs text-center text-green-600`}>
+                      Generated Successfully
+                    </Text>
+                  )}
+                  <ImageBackground
+                    style={tw`relative w-full overflow-hidden rounded-xl shadow-md shadow-red-600`}
+                    resizeMode="cover"
                     source={{
                       uri: capturedImage,
-                    }}
-                  />
+                    }}>
+                    <View style={tw`absolute w-full h-full bg-black opacity-90`} />
+                    <Image
+                      style={tw`w-full h-[26.7rem]`}
+                      resizeMode="contain"
+                      source={{
+                        uri: capturedImage,
+                      }}
+                    />
+                    <TouchableOpacity
+                      style={tw`absolute top-3 right-3 p-2 rounded-full bg-white bg-opacity-70`}
+                      onPress={handleResetAll}>
+                      <FeatherIcon name="x" size={20} color="#222" />
+                    </TouchableOpacity>
+                  </ImageBackground>
+                </>
+              ) : (
+                <>
+                  <Text style={tw`font-poppins text-xs text-neutral-600`}>
+                    Capture a photo with a words or paragraph
+                  </Text>
                   <TouchableOpacity
-                    style={tw`absolute top-3 right-3 p-2 rounded-full bg-white bg-opacity-70`}
-                    onPress={handleResetAll}>
-                    <FeatherIcon name="x" size={20} color="#222" />
+                    style={tw`flex-col items-center justify-center w-full h-[20rem] rounded-3xl shadow-md shadow-red-600 border border-accent-2 bg-accent-3`}
+                    onPress={handleCapturePhoto}>
+                    <FeatherIcon name="camera" size={100} color="#FFB0B0" />
                   </TouchableOpacity>
-                </ImageBackground>
-              </>
-            ) : (
-              <>
-                <Text style={tw`font-poppins text-xs text-neutral-600`}>
-                  Capture a photo with a words or paragraph
-                </Text>
-                <TouchableOpacity
-                  style={tw`flex-col items-center justify-center w-full h-[20rem] rounded-3xl shadow-md shadow-red-600 border border-accent-2 bg-accent-3`}
-                  onPress={handleCapturePhoto}>
-                  <FeatherIcon name="camera" size={100} color="#FFB0B0" />
-                </TouchableOpacity>
-                <Text style={tw`font-poppins text-xs text-neutral-600`}>or</Text>
-                <TouchableOpacity
-                  style={tw`flex-row items-center justify-center w-full p-3 gap-x-2 rounded-xl shadow-md shadow-red-600 border border-accent-1 bg-accent-3`}
-                  onPress={handleChoosePhoto}>
-                  <FeatherIcon name="image" size={14} color="#555" />
-                  <Text style={tw`font-poppins text-xs text-neutral-600`}>Browse Gallery</Text>
-                </TouchableOpacity>
-              </>
-            )}
-          </View>
-          <TextEditor
+                  <Text style={tw`font-poppins text-xs text-neutral-600`}>or</Text>
+                  <TouchableOpacity
+                    style={tw`flex-row items-center justify-center w-full p-3 gap-x-2 rounded-xl shadow-md shadow-red-600 border border-accent-1 bg-accent-3`}
+                    onPress={handleChoosePhoto}>
+                    <FeatherIcon name="image" size={14} color="#555" />
+                    <Text style={tw`font-poppins text-xs text-neutral-600`}>Browse Gallery</Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </View>
+            <TextEditor
+              initialText={recognizedTexts?.text}
+              richTextHandle={richTextHandle}
+              handleClear={handleResetAll}
+              handleSave={() => setIsVisibleSaveModal(true)}
+            />
+          </DefaultLayout>
+          <SavePDFModal
             isSaving={isSavingPDF}
-            initialText={recognizedTexts?.text}
-            richTextHandle={richTextHandle}
-            handleClear={handleResetAll}
-            handleSave={handleSave}
+            fileName={fileName}
+            setFileName={setFileName}
+            isVisible={isVisibleSaveModal}
+            setIsVisible={setIsVisibleSaveModal}
+            onSave={handleSave}
           />
-        </DefaultLayout>
+        </>
       )}
     </>
   );
